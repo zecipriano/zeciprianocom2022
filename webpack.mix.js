91ccb0c9380eda9b7ca9b4abe3d0ce3a409f8b7e
Zé Cipriano <ze@zecipriano.com>
2022-03-17 19:09:28 +0000

    Remove default JavaScript file (not used)

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,6 @@ mix.disableNotifications();
 mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
-    .js('source/_assets/js/main.js', 'js')
     .css('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
         require('tailwindcss'),
