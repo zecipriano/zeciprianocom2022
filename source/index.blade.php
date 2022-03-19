550c4524fca04e9ec2120982869a6668d9936699
Zé Cipriano <ze@zecipriano.com>
2022-03-19 14:34:48 +0000

    Style curriculum vitae links

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.main')
 
 @section('body')
-<div class="p-8 border-2 border-primary max-w-5xl grid grid-cols-1 sm:grid-cols-golden gap-10">
+<div class="p-8 border-2 border-primary max-w-5xl grid grid-cols-1 sm:grid-cols-golden gap-8">
     <div class="flex flex-col items-center justify-center gap-6">
         <img alt="Fotografia de Zé Cipriano" src="{{ $page->baseUrl }}/assets/images/photo.jpg" class="rounded-full w-1/2">
         <img alt="Zé Cipriano" src="{{ $page->baseUrl }}/assets/images/logo.svg" class="w-4/5 sm:w-full md:w-5/6">
@@ -12,9 +12,7 @@
 
         @include('_partials.contacts')
 
-        <div class="mb-8">
-            <p>Curriculum Vitae (pdf): <a href="{{ $page->baseUrl }}/assets/files/curriculum_vitae__ze_cipriano__pt.pdf" target="__blank">[Português]</a> <a href="{{ $page->baseUrl }}/assets/files/curriculum_vitae__ze_cipriano__en.pdf" target="__blank">[English]</a></p>
-        </div>
+        @include('_partials.cv')
 
         @include('_partials.links')
     </div>
