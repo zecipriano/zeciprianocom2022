748d71cbc1e4b358f796188e8bc8211b23289252
Zé Cipriano <ze@zecipriano.com>
2022-03-19 14:38:25 +0000

    Adjust intro text size and photo/logo position

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,13 +2,13 @@
 
 @section('body')
 <div class="p-8 border-2 border-primary max-w-5xl grid grid-cols-1 sm:grid-cols-golden gap-8">
-    <div class="flex flex-col items-center justify-center gap-6">
+    <div class="flex flex-col items-center md:justify-center gap-6">
         <img alt="Fotografia de Zé Cipriano" src="{{ $page->baseUrl }}/assets/images/photo.jpg" class="rounded-full w-1/2">
         <img alt="Zé Cipriano" src="{{ $page->baseUrl }}/assets/images/logo.svg" class="w-4/5 sm:w-full md:w-5/6">
     </div>
 
     <div>
-        <p class="text-lg mb-8">Programador web com mais de 10 anos de experiência como freelancer / independente, especializado em PHP e Laravel.</p>
+        <p class="md:text-lg mb-8">Programador web com mais de 10 anos de experiência como freelancer / independente, especializado em PHP e Laravel.</p>
 
         @include('_partials.contacts')
 
